42cfc6cbb9949d9c8f5cb530d012977b73095554
Maicol Battistini <maicolbattistini@live.it>
2025-03-29 16:28:26 +0100

    fix(catalog): Add missing imports for card and data table

--- catalog/src/hydration-entrypoints/components/card.ts
+++ catalog/src/hydration-entrypoints/components/card.ts
@@ -7,3 +7,7 @@
 import '@maicol07/material-web-additions/card/elevated-card.js';
 import '@maicol07/material-web-additions/card/filled-card.js';
 import '@maicol07/material-web-additions/card/outlined-card.js';
+
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/iconbutton/icon-button.js';
--- catalog/src/hydration-entrypoints/components/data-table.ts
+++ catalog/src/hydration-entrypoints/components/data-table.ts
@@ -9,3 +9,5 @@ import '@maicol07/material-web-additions/data-table/data-table-column.js';
 import '@maicol07/material-web-additions/data-table/data-table-row.js';
 import '@maicol07/material-web-additions/data-table/data-table-cell.js';
 import '@maicol07/material-web-additions/data-table/data-table-footer.js';
+
+import '@material/web/button/text-button.js';
