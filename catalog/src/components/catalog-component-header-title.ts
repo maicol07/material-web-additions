2d3bf6dbd43f683dbc057637b5541df18d851678
Maicol Battistini <maicolbattistini@live.it>
2025-04-04 00:56:37 +0200

    refactor(catalog): ♻️ Refactor component header styles
    This commit refactors the catalog component header and title styles for better consistency and readability.
    
    - Removes `override` keyword from render and styles properties in `CatalogComponentHeader` and `CatalogComponentHeaderTitle` as it is not needed.
    - Minor formatting changes in styles to improve readability.
    - No functional changes are introduced.

--- catalog/src/components/catalog-component-header-title.ts
+++ catalog/src/components/catalog-component-header-title.ts
@@ -15,46 +15,46 @@ import {customElement} from 'lit/decorators.js';
  */
 @customElement('catalog-component-header-title')
 export class CatalogComponentHeaderTitle extends LitElement {
-  override render() {
+  render() {
     return html`<slot></slot>`;
   }
 
-  static override styles = css`
-    :host {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      border-radius: var(--catalog-image-border-radius);
-      padding: 56px;
-      /* It has no border so we need it to be a different color from the top app
-       * bar and nav drawer.
-       */
-      background-color: var(--md-sys-color-surface-variant);
-      color: var(--md-sys-color-on-surface-variant);
-    }
-
-    slot::slotted(h1) {
-      margin-block-end: 8px;
-      font-weight: 475;
-    }
-
-    slot::slotted(p) {
-      font-size: var(--catalog-title-l-font-size);
-    }
-
-    slot::slotted(*) {
-      margin-block-start: 0;
-    }
-
-    slot::slotted(*:last-child) {
-      margin-block-end: 0;
-    }
-
-    @media screen and (max-width: 600px) {
+  static styles = css`
       :host {
-        padding: 32px;
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          border-radius: var(--catalog-image-border-radius);
+          padding: 56px;
+          /* It has no border so we need it to be a different color from the top app
+           * bar and nav drawer.
+           */
+          background-color: var(--md-sys-color-surface-variant);
+          color: var(--md-sys-color-on-surface-variant);
       }
-    }
+
+      slot::slotted(h1) {
+          margin-block-end: 8px;
+          font-weight: 475;
+      }
+
+      slot::slotted(p) {
+          font-size: var(--catalog-title-l-font-size);
+      }
+
+      slot::slotted(*) {
+          margin-block-start: 0;
+      }
+
+      slot::slotted(*:last-child) {
+          margin-block-end: 0;
+      }
+
+      @media screen and (max-width: 600px) {
+          :host {
+              padding: 32px;
+          }
+      }
   `;
 }
--- catalog/src/components/catalog-component-header.ts
+++ catalog/src/components/catalog-component-header.ts
@@ -16,90 +16,87 @@ import {customElement} from 'lit/decorators.js';
  */
 @customElement('catalog-component-header')
 export class CatalogComponentHeader extends LitElement {
-  override render() {
-    return html`
-      <div>
-        <slot class="title" name="title"></slot>
-        <slot
-          class="image"
-        ></slot>
-      </div>`;
+  render() {
+    return html` <div>
+      <slot class="title" name="title"></slot>
+      <slot class="image"></slot>
+    </div>`;
   }
 
-  static override styles = css`
-    :host {
-      display: block;
-      --catalog-image-border-radius: var(--catalog-shape-xl);
-      container: host / inline-size;
-      position: relative;
-      margin-inline: auto;
-    }
+  static styles = css`
+      :host {
+          display: block;
+          --catalog-image-border-radius: var(--catalog-shape-xl);
+          container: host / inline-size;
+          position: relative;
+          margin-inline: auto;
+      }
 
-    slot {
-      height: 100%;
-    }
+      slot {
+          height: 100%;
+      }
 
-    slot,
-    .image::slotted(*) {
-      display: flex;
-      align-items: start;
-      margin: 0;
-    }
+      slot,
+      .image::slotted(*) {
+          display: flex;
+          align-items: start;
+          margin: 0;
+      }
 
-    .end slot,
-    .end .image::slotted(*) {
-      align-items: end;
-    }
+      .end slot,
+      .end .image::slotted(*) {
+          align-items: end;
+      }
 
-    .center slot,
-    .center .image::slotted(*) {
-      align-items: center;
-    }
-
-    .image {
-      /* Color needs to differ from top-app-bar and sidebar */
-      background-color: var(--md-sys-color-surface-variant);
-      border-radius: var(--catalog-image-border-radius);
-      overflow: hidden;
-      margin-block-start: 16px;
-      aspect-ratio: 3 / 2;
-      max-width: 100%;
-    }
-
-    ::slotted(*) {
-      box-sizing: border-box;
-      height: 100%;
-      width: 100%;
-    }
-
-    .image::slotted(*) {
-      --catalog-image-border-radius: 0px;
-      --catalog-header-image-height: 100%;
-      --catalog-header-image-width: auto;
-      display: flex;
-      justify-content: center;
-    }
-
-    /* fit ipad pro at least */
-    @container (width > 1000px) {
-      div {
-        display: grid;
-        grid-gap: 8px;
-        gap: 8px;
-        grid-template-columns: repeat(2, 1fr);
-        grid-auto-flow: row;
+      .center slot,
+      .center .image::slotted(*) {
+          align-items: center;
       }
 
       .image {
-        margin-block-start: 0;
-        aspect-ratio: unset;
+          /* Color needs to differ from top-app-bar and sidebar */
+          background-color: var(--md-sys-color-surface-variant);
+          border-radius: var(--catalog-image-border-radius);
+          overflow: hidden;
+          margin-block-start: 16px;
+          aspect-ratio: 3 / 2;
+          max-width: 100%;
       }
 
-      .image,
-      .image::slotted(*) {
-        min-height: 100%;
-        max-height: 544px;
+      ::slotted(*) {
+          box-sizing: border-box;
+          height: 100%;
+          width: 100%;
       }
-    }
+
+      .image::slotted(*) {
+          --catalog-image-border-radius: 0px;
+          --catalog-header-image-height: 100%;
+          --catalog-header-image-width: auto;
+          display: flex;
+          justify-content: center;
+      }
+
+      /* fit ipad pro at least */
+      @container (width > 1000px) {
+          div {
+              display: grid;
+              grid-gap: 8px;
+              gap: 8px;
+              grid-template-columns: repeat(2, 1fr);
+              grid-auto-flow: row;
+          }
+
+          .image {
+              margin-block-start: 0;
+              aspect-ratio: unset;
+          }
+
+          .image,
+          .image::slotted(*) {
+              min-height: 100%;
+              max-height: 544px;
+          }
+      }
   `;
 }
