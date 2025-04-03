7e79cf66dc2129775b05e0526799fa32bf153304
Maicol Battistini <maicolbattistini@live.it>
2025-04-04 00:56:52 +0200

    fix(ssr): 🐛 Enable SSR for key components
    Re-enable server-side rendering for essential catalog components to improve initial load performance and SEO.
    
    - Uncomments necessary import statements.
    - Fixes issue where components were not being SSRd.
    - Includes catalog-component-header, nav-drawer and item.

--- catalog/src/ssr.ts
+++ catalog/src/ssr.ts
@@ -4,16 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// import '@lit-labs/ssr/lib/install-global-dom-shim.js';
+import '@lit-labs/ssr/lib/install-global-dom-shim.js';
 
 // This file imports only files that will be SSRd e.g. if you can't SSR a
 // component, don't import it here.
-// import './components/catalog-component-header.js';
-// import './components/catalog-component-header-title.js';
-// import './components/top-app-bar.js';
-// import './components/nav-drawer.js';
-// import './components/theme-changer.js';
-// import '@material/web/all.js';
-// import './components/drag-playground.js';
-// 🤫
-// import '@material/web/labs/item/item.js';
+
+// import('@material/web/all.js');
+import('./components/catalog-component-header.js');
+import('./components/catalog-component-header-title.js');
+import('./components/nav-drawer.js');
+// import('./components/theme-changer.js');
+// import('./components/top-app-bar.js');
+import('./components/drag-playground.js');
+import('@material/web/labs/item/item.js');
